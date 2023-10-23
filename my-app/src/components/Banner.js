5b31acedb6d5f830ac78c8d93ce21d5edbf3966c
Alison Coutinho <93990888+TinhoLima@users.noreply.github.com>
2023-10-23 08:06:01 -0300

    modificações

--- my-app/src/components/Banner.js
+++ my-app/src/components/Banner.js
@@ -28,7 +28,6 @@ export const Banner = () => {
                         />
                         <p>Sejam muito bem-vindos! Meu nome é Alison Coutinho e sou apaixonado por desenvolvimento e por toda a área de tecnologia. Nesta página você vai encontrar meus projetos pessoais, minhas skills e minhas informações de contato.</p>
                         <a  href={pdf} download="file-pdf"><button className="banner-cv">Download CV<ArrowRightCircle size={20}/></button></a>
-                        {/* <button className="banner-cv"><a href={pdf} download="file-pdf">Download CV<ArrowRightCircle size={20}/></a></button> */}
                     </Col>
                     <Col xs={12} md={6} xl={5}>
                         <img src={headerImg1} alt="Headder Img" />
